17ba31faf2edda087de9644bc3306267c4fdf21d
mohammadraddad <d.dodo.dode@hotmail.com>
2021-06-09 10:34:00 +0300

    name

---
+++ fund/Name.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package traffic;
+
+//Fibonacci Series using Recursion
+class Traffic
+{
+
+	
+	public static void main (String args[])
+	{
+	
+	System.out.println("my name is mohammad ");
+        System.out.println("i am 20 years old ");
+        System.out.println("my home town is salfeet ");
+	}
+}
+/* This code is contributed by Rajat Mishra */
