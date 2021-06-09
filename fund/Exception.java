6cdef4a863ca418b403e772c1ade00402d23c368
mohammadraddad <d.dodo.dode@hotmail.com>
2021-06-09 21:42:31 +0300

    Exception

---
+++ fund/Exception.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.HashMap;
+
+import javax.print.attribute.HashAttributeSet;
+public class job_que {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Object> myList = new ArrayList<Object>();
+		myList.add("13");
+		myList.add("hello world");
+		myList.add(48);
+		myList.add("Goodbye World");
+		try {
+		for(int i = 0; i < myList.size(); i++) {
+		    Integer castedValue = (Integer) myList.get(i);
+		}
+		} catch(Exception exception) {
+			System.out.println("enrter a string not int");
+		}
+
+         
+		 
+	
+
+	}
+	
+
+
+}
