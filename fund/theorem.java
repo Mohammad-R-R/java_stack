94680c5da81d9d45252aeb1ed357f2121d95e1d2
mohammadraddad <d.dodo.dode@hotmail.com>
2021-06-09 11:00:07 +0300

    theorem

---
+++ fund/theorem.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package traffic;
+import static java.lang.Math.*;
+class Pythagorean_Theorem
+{
+
+	
+	public static void main (String args[])
+	{
+
+            int x=5;
+            int b=6;
+            int c =0;
+            c=(int) Math.sqrt((x*x)+(b*b));
+            System.out.print(c);
+        
+	}
+}
+/* This code is contributed by Rajat Mishra */
