c37f3d73da5461eb77fb7a0ce96d95895840208d
mohammadraddad <d.dodo.dode@hotmail.com>
2021-06-10 04:53:16 +0300

    first oop

---
+++ oop/Project.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.HashMap;
+
+import javax.print.attribute.HashAttributeSet;
+public class Project {
+	String  name;
+	String  description;
+	
+	public Project() {
+		
+	}
+	public Project (String name) {
+		this.name=name;
+	}
+	public Project (String name ,String description ) {
+		this.name=name;
+		this.description=description;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public static void main(String[] args) {
+		
+		
+		
+		}
+	}
+	
+
+
+
